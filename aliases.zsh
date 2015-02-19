7dd93d29b694f5a3740d79b7dc450fcaf6899de1
Kaung Htet Zaw <emoosx@gmail.com>
2015-02-19 17:36:09 +0800

    Flush DNS in Yosemite

--- aliases.zsh
+++ aliases.zsh
@@ -37,7 +37,7 @@ alias update='sudo softwareupdate -i -a; brew update; brew cask update; brew upg
 # IP addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"
-alias flush="dscacheutil -flushcache"
+alias flush="sudo discoveryutil udnsflushcaches"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
 
 # Clean up LaunchServices to remove duplicates in the “Open With” menu
